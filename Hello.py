24267a7aebbdbdcff59880c33accd4b407b9b3fb
Dharanish111 <168800443+Dharanish111@users.noreply.github.com>
2024-05-10 14:42:10 +0530

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+st.write("# Hello👋,Welcome to the Ownrecipe!")
+
+st.header("     Here you can make your favourite dish,in a easy step by step process followed by our instructions.")
+
